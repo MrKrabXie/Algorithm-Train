df6ee56057f14a5a119985e0bfc2f4a77880db9a
谢嘉伟 <wei17306927526@gmail.com>
2024-06-14 13:32:13 +0800

    Add algorithm

--- SearchAlgorithm/SearchAlgorithm.py
+++ SearchAlgorithm/SearchAlgorithm.py
@@ -1,8 +1,21 @@
+import time
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+def performance_monitor(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Function {func.__name__} executed in {execution_time} seconds")
+        return result
+    return wrapper
+
+import random
+
+# 生成几百万个随机数作为测试数据集生成不同的序列， 每个
+random_sequences = random.sample(range(1000000), 10000)
+
 
 if __name__ == '__main__':
     unittest.main()
